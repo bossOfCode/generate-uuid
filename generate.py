0de9f57bab33bebda94ba8e67182022c51ba9112
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-22 16:23:34 -0500

    fix errors pt 2/3

--- generate.py
+++ generate.py
@@ -7,32 +7,27 @@ namespace = os.environ.get("INPUTS_NAMESPACE")
 
 versions = [1, 3, 4, 5]
 
-if version >= 1 or version <= 8:
-  if version == 1:
-    output = uuid.uuid1()
-  else:
-    if version == 2:
-      output = uuid.uuid2()
+def printInOs(input):
+    print(f"{input}")
+    os.system(f"{input}")
+
+if version in versions:
+    if version == 1:
+        output = uuid.uuid1()
     else:
-      if version == 3:
-        output = uuid.uuid3(namespace)
-      else:
-        if version == 4:
-          output = uuid.uuid4()
+        if version == 3:
+            output = uuid.uuid3(namespace)
         else:
-          if version == 5:
-            output = uuid.uuid5(namespace)
-          else:
-            if version == 6:
-              output = uuid.uuid6()
+            if version == 4:
+                output = uuid.uuid4()
             else:
-              if version == 7:
-                output = uuid.uuid7()
-              else:
-                if version == 8:
-                  output = uuid.uuid8()
+                if version == 5:
+                    output = uuid.uuid5(namespace)
 else:
-  exit(f"ERROR: version {version} does not exist")
+    exit(f"ERROR: version {version} does not exist")
 
 output = str(output)
-print(f"::set-output name=uuid::{output}")
+printInOs(f"::set-output name=uuid::{output}")
+
+printInOs(f"Generated UUID {output}")
+
