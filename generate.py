941ac7b064fc029266fe577beb4c0f669c81b74a
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-23 15:07:50 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,7 +6,6 @@ import uuid
 version = os.environ.get("INPUTS_UUID")
 namespace = os.environ.get("INPUTS_NAMESPACE")
 name = os.environ.get("INPUTS_NAME")
-output = ""
 
 versions = [1, 3, 4, 5]
 namespaces = ["DNS", "URL", "OID", "X500"]
@@ -18,16 +17,16 @@ def print_in_os(argument):
 
 if version in versions or namespace in namespaces:
     if version == 1:
-        output = uuid.uuid1()
+        output = str(uuid.uuid1())
     else:
         if version == 3:
-            output = uuid.uuid3(str("NAMESPACES_" + namespace), name)
+            output = str(uuid.uuid3(str("NAMESPACES_" + namespace), name))
         else:
             if version == 4:
-                output = uuid.uuid4()
+                output = str(uuid.uuid4())
             else:
                 if version == 5:
-                    output = uuid.uuid5(str("NAMESPACES_" + namespace), name)
+                    output = str(uuid.uuid5(str("NAMESPACES_" + namespace), name))
 else:
     if namespace in namespaces:
         sys.exit(f"ERROR: namespace cannot be {namespace}; must be either DNS, URL, OID, or X500.")
@@ -35,10 +34,9 @@ else:
         if version in versions:
             sys.exit(f"ERROR: version {version} does not exist")
 
-FINAL = str(output)
 SAFE = uuid.SafeUUID
 
-print_in_os(f"echo 'uuid={FINAL}' >> $GITHUB_OUTPUT")
+print_in_os(f"echo 'uuid={output}' >> $GITHUB_OUTPUT")
 print_in_os(f"echo 'safe={SAFE}' >> $GITHUB_OUTPUT")
 
 if SAFE == "unsafe":
