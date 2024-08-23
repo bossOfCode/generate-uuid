ece4ea2bafe41b10ea5151fd6079f8db10058b23
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-22 21:17:39 -0500

    Update generate.py
    Fix typo in warning
    Updated output setting
    Output variable is now called final

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ import uuid
 version = os.environ.get("INPUTS_UUID")
 namespace = os.environ.get("INPUTS_NAMESPACE")
 name = os.environ.get("INPUTS_NAME")
-OUTPUT = ""
+output = ""
 
 versions = [1, 3, 4, 5]
 namespaces = ["DNS", "URL", "OID", "X500"]
@@ -18,16 +18,16 @@ def print_in_os(argument):
 
 if version in versions or namespace in namespaces:
     if version == 1:
-        OUTPUT = uuid.uuid1()
+        output = uuid.uuid1()
     else:
         if version == 3:
-            OUTPUT = uuid.uuid3(str("NAMESPACES_" + namespace), name)
+            output = uuid.uuid3(str("NAMESPACES_" + namespace), name)
         else:
             if version == 4:
-                OUTPUT = uuid.uuid4()
+                output = uuid.uuid4()
             else:
                 if version == 5:
-                    OUTPUT = uuid.uuid5(str("NAMESPACES_" + namespace), name)
+                    output = uuid.uuid5(str("NAMESPACES_" + namespace), name)
 else:
     if namespace in namespaces:
         sys.exit(f"ERROR: namespace cannot be {namespace}; must be either DNS, URL, OID, or X500.")
@@ -35,14 +35,14 @@ else:
         if version in versions:
             sys.exit(f"ERROR: version {version} does not exist")
 
-OUTPUT = str(OUTPUT)
+FINAL = str(output)
 SAFE = uuid.SafeUUID
 
-print_in_os(f"::set-output name=uuid::{OUTPUT}")
-print_in_os(f"::set-output name=safe::{SAFE}")
+print_in_os(f"::uuid={FINAL}::")
+print_in_os(f"::safe={SAFE}::")
 
 if SAFE != "safe":
-    print_in_os("::warning title=UNSAFE::Your UUID may be ubsafe for public use because" +
+    print_in_os("::warning title=UNSAFE::Your UUID may be unsafe for public use because" +
                 " it may contain some of your device's personal info.")
 
-print_in_os(f"Generated UUID {OUTPUT}")
+print_in_os(f"Generated UUID {FINAL}")
