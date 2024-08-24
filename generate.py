ecb52830ebd1df64554f63fdddcc7829cd634649
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-08-24 09:52:08 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -7,8 +7,7 @@ VERSION = os.environ.get("INPUT_VERSION")
 namespace = os.environ.get("INPUT_NAMESPACE")
 name = os.environ.get("INPUT_NAME")
 
-OUTPUT = ""
-VERSION = str(getattr(uuid, f"uuid{VERSION}"))
+VERSION = getattr(uuid, f"uuid{VERSION}")
 
 versions = ["uuid1", "uuid3", "uuid4", "uuid5"]
 namespaces = ["DNS", "URL", "OID", "X500"]
@@ -21,7 +20,7 @@ def print_in_os(argument):
 print_in_os(f"Version: {VERSION}")
 
 if VERSION in versions or namespace in namespaces:
-    OUTPUT = str(VERSION())
+    OUTPUT = VERSION()
 else:
     if namespace in namespaces:
         sys.exit(f"ERROR: namespace cannot be {namespace}; must be either DNS, URL, OID, or X500.")
@@ -29,10 +28,12 @@ else:
         if VERSION in versions:
             sys.exit(f"ERROR: VERSION {VERSION} does not exist")
 
-SAFE = uuid.SafeUUID
+OUTPUTSTR = str(OUTPUT)
+FINAL = uuid.UUID(OUTPUTSTR)
+SAFE = str(uuid.SafeUUID)
 
-print_in_os(f"echo 'uuid={OUTPUT}' >> $GITHUB_OUTPUT")
-print_in_os(f"echo 'safe={str(SAFE)}' >> $GITHUB_OUTPUT")
+print_in_os(f"echo 'uuid={FINAL}' >> $GITHUB_OUTPUT")
+print_in_os(f"echo 'safe={SAFE}' >> $GITHUB_OUTPUT")
 
 if SAFE == "unsafe":
     print_in_os("::warning title=UNSAFE::Your UUID may be unsafe for public use because" +
