aa20af5f11216c00c695e0c14bd254b1220e3a87
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2024-09-01 14:48:41 -0500

    Update generate7.py

--- src/generate7.py
+++ src/generate7.py
@@ -1,1 +1,31 @@
+import os
+import sys
+import time
+import secrets as hex
 
+def insert(str1, str2, index):
+  orginal = str1
+  new = str2
+  pos = index
+
+  global output
+  output = original[:pos] + new + original[pos:]
+
+uuid = ''
+next = ''
+
+epoch = format(time.time() * 1000, 'x')
+
+uuid = '0' + epoch + '-'
+
+insert(uuid, '-', 7)
+uuid = output + '7'
+
+next = hex.token_hex(1.5)
+uuid = uuid + next + '-b'
+
+next = hex.token_hex(1.5)
+uuid = uuid + next + '-'
+
+next = hex.token_hex(6)
+uuid = uuid + next
